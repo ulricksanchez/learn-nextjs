b207ae6790b62651ee3b339174ad4eef1ac7a955
Ulrick Sanchez <ulricksanchez@gmail.com>
2025-07-02 18:47:58 +0800

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Set this to false if you want production builds to abort if there's type errors
     ignoreBuildErrors: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
